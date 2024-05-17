f02409e322ba7214dafd6d1d3d199c71966889b7
Mikhail Chystsiakou <mikhail.chystsiakou@gmail.com>
2024-05-17 17:41:02 +0300

    fix: re-created protos

--- examples/logging/index.ts
+++ examples/logging/index.ts
@@ -1,11 +1,11 @@
 import { AstralineClient } from "@astraline/streaming-client";
 
-const client = new AstralineClient('key_2');
+const client = new AstralineClient('key_1');
 
-client.registerProcessedTxCallback('4dv4puYib9XZXHrTN5csFxzfWKSmetTu1dLsJWNNV2XZ', (tx) => {
+client.registerProcessedTxCallback('72i21TqCQw6oTGULXHNmuHkyrzyjbsGVdem1f4mUnAMJ', (tx) => {
   console.log(`Processed tx received: ${JSON.stringify(tx)}\n`);
 });
 
-client.registerUnprocessedTxCallback('4dv4puYib9XZXHrTN5csFxzfWKSmetTu1dLsJWNNV2XZ', (tx) => {
+client.registerUnprocessedTxCallback('72i21TqCQw6oTGULXHNmuHkyrzyjbsGVdem1f4mUnAMJ', (tx) => {
   console.log(`Unprocessed tx received: ${JSON.stringify(tx)}\n`);
 });

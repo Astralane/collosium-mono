8a3bc5b2f15df96a11c2d959d47e3364fd00e610
Artem Shkonda <artemshkonda@MacBook-Air-Artem.local>
2024-05-17 17:36:14 +0300

    update branch

--- examples/logging/index.ts
+++ examples/logging/index.ts
@@ -1,7 +1,6 @@
-// import { client } from "@astraline/streaming-client";
-import { AstralineClient } from '../../dist/index';
+import { AstralineClient } from "@astraline/streaming-client";
 
-const client = new AstralineClient('key_3');
+const client = new AstralineClient('key_2');
 
 client.registerProcessedTxCallback('4dv4puYib9XZXHrTN5csFxzfWKSmetTu1dLsJWNNV2XZ', (tx) => {
   console.log(`Processed tx received: ${JSON.stringify(tx)}\n`);

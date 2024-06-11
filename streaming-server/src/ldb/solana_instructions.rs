eb9c284d5b69e2243e134bf37827f05bbf8af18c
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2024-06-11 21:22:28 +0300

    feat: add support for creating tables (insturction_name and account_<account name>)

--- streaming-server/src/ldb/solana_instructions.rs
+++ streaming-server/src/ldb/solana_instructions.rs
@@ -273,7 +273,23 @@ pub(crate) fn column_type(column_name: &str) -> Result<String> {
         "account_arguments" => Ok(String::from("text[]")),
         "tx_signer" => Ok(String::from("text")),
         "tx_success" => Ok(String::from("boolean")),
+        "instruction_name" => Ok(String::from("text")),
+        _ => match_by_pattern(column_name),
+    }
+}
+
+fn match_by_pattern(column_name: &str) -> Result<String> {
+    match get_pattern(column_name) {
+        "account_*" => Ok(String::from("text")),
         _ => Err(format!("Unknown column name {column_name}")),
+    }
+}
+
+fn get_pattern(column_name: &str) -> &str {
+    if column_name.starts_with("account_") {
+        "account_*"
+    } else {
+        ""
     }
 }
 

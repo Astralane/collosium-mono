194a8f08181b2bd666f1f02a98ec0691c48425b3
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2024-09-06 02:17:44 +0530

    imporve logs

--- mev-sink/src/main.rs
+++ mev-sink/src/main.rs
@@ -7,6 +7,8 @@ use crate::sf::solana;
 use crate::substream_client::SubstreamClient;
 use clap::Parser;
 use std::sync::Arc;
+use tracing::Level;
+use tracing_subscriber::fmt::format::FmtSpan;
 
 include!("pb/pb.rs");
 
@@ -37,14 +39,17 @@ struct Args {
 }
 #[tokio::main]
 async fn main() {
-    let subscriber = tracing_subscriber::FmtSubscriber::new();
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    tracing_subscriber::fmt()
+        .compact()
+        .with_thread_ids(true)
+        // sets this to be the default, global subscriber for this application.
+        .init();
 
     let start = std::time::Instant::now();
     tracing::info!("Starting MEV sink");
 
     let args = Args::parse();
-    tracing::info!("syncing blocks from {} to {}", args.from, args.to);
+    tracing::info!("syncing blocks from {} to {}, {} blocks", args.from, args.to, args.to - args.from);
 
     let clickhouse = clickhouse::Client::default()
         .with_url(args.db_url)
@@ -80,10 +85,12 @@ async fn main() {
     sandwiches_job.await.expect("error in streaming tips");
     tips_job.await.expect("error in streaming sandwiches");
 
+    tracing::info!("all stream finished in {:?}", start.elapsed());
+
     //all jobs finished sending, call shutdown
     client_handle.shutdown().await;
 
-    tracing::info!("all stream finished in {:?}", start.elapsed());
+    tracing::info!("program finished in {:?}", start.elapsed());
     //sleep for 10s for tasks in database background to finish
     tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
 }
